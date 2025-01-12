ff8064d2f045b7254706629e81634665b54adaad
Mike <michael.pineau89@gmail.com>
2025-01-12 22:00:58 +0000

    updated dag to a taskflow instead of a mix of taskflow and traditional dag

--- dags/load_files.py
+++ dags/load_files.py
@@ -3,23 +3,23 @@ import datetime
 
 from airflow.models import Variable
 from airflow.utils.task_group import TaskGroup
-from airflow import DAG, task
+from airflow import DAG
+from airflow.decorators import task, dag
 from airflow.providers.google.cloud.transfers import gcs_to_bigquery
 
-
-
-@task(task_id = 'get_raw_file')
-def get_raw_file(quarter: int) -> pd.DataFrame:
-    return pd.read_excel(Variable('test_gs_bucket') + f'raw_excel_lmia/tfwp_2024q{quarter}_pos_en.xlsx', sheet_name='2024_POS_EN', skipfooter=8)
-
-@task(task_id = 'save_lmia_df_as_csv')
-def save_lmia_df_as_csv(df, quarter: int) -> None:
-    df.to_csv(Variable('test_gs_bucket') + f'transformed_csv/lmia_q{quarter}.csv', index = False)
-
-with DAG(dag_id = 'load_lmia_file',
+@dag(dag_id = 'load_lmia_file',
 start_date = datetime.date(2025,1,11),
-schedule = "@once"
-):
+schedule = "@once")
+def load_files():
+    @task(task_id = 'get_raw_file')
+    def get_raw_file(quarter: int) -> pd.DataFrame:
+        return pd.read_excel(Variable('test_gs_bucket') + f'raw_excel_lmia/tfwp_2024q{quarter}_pos_en.xlsx', sheet_name='2024_POS_EN', skipfooter=8)
+
+    @task(task_id = 'save_lmia_df_as_csv')
+    def save_lmia_df_as_csv(df, quarter: int) -> None:
+        df.to_csv(Variable('test_gs_bucket') + f'transformed_csv/lmia_q{quarter}.csv', index = False)
+
+
     with TaskGroup('load_data') as raw:
         for quarter in range(1,4):
             get_raw = get_raw_file.override(task_id = f'get_raw_q{quarter}')(quarter)
@@ -34,4 +34,4 @@ schedule = "@once"
             )
             get_raw >> load_raw_to_csv >> load_csv_to_bq
     
-        
+load_files()
