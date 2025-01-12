570784a0c7f59d30cd402f47f584737a34298291
Mike <michael.pineau89@gmail.com>
2025-01-12 21:49:50 +0000

    finished initial load process

--- dags/load_files.py
+++ dags/load_files.py
@@ -1,22 +1,36 @@
-from airflow import DAG
-import datetime
-from airflow.operators.python import PythonOperator
-from airflow.models import Variable
 import pandas as pd
+import datetime
 
+from airflow.models import Variable
+from airflow.utils.task_group import TaskGroup
+from airflow import DAG, task
+from airflow.providers.google.cloud.transfers import gcs_to_bigquery
+
+
+
+@task(task_id = 'get_raw_file')
 def get_raw_file(quarter: int) -> pd.DataFrame:
     return pd.read_excel(Variable('test_gs_bucket') + f'raw_excel_lmia/tfwp_2024q{quarter}_pos_en.xlsx', sheet_name='2024_POS_EN', skipfooter=8)
 
-def save_df_as_csv(df, quarter: int) -> None:
+@task(task_id = 'save_lmia_df_as_csv')
+def save_lmia_df_as_csv(df, quarter: int) -> None:
     df.to_csv(Variable('test_gs_bucket') + f'transformed_csv/lmia_q{quarter}.csv', index = False)
-
-
-
 
 with DAG(dag_id = 'load_lmia_file',
 start_date = datetime.date(2025,1,11),
 schedule = "@once"
 ):
-    for i in range(1,4):
-        get_raw = PythonOperator(python_callable=get_raw_file)
-        load_raw = PythonOperator(python_callable=save_df_as_csv,)
+    with TaskGroup('load_data') as raw:
+        for quarter in range(1,4):
+            get_raw = get_raw_file.override(task_id = f'get_raw_q{quarter}')(quarter)
+            load_raw_to_csv = save_lmia_df_as_csv.override(task_id = f'save_lmia_df_as_csv_q{quarter}')("{{ti.xcomm_pull('raw_data.get_raw_q{quarter})}}")
+            load_csv_to_bq = gcs_to_bigquery(
+                bucket = Variable('tst_gs_bucket'),
+                source_object = '/transformed_csv/lmia_q{quarter}.csv',
+                destination_project_database_table = 'lmia.lmia_applications_raw',
+                source_format = 'csv',
+                autodetect = True # being explicit here although this is true by default as per docs
+            )
+            get_raw >> load_raw_to_csv >> load_csv_to_bq
+    
+        
