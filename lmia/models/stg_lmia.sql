fa7fd7b74e6e04eba9ee6a9dcfb861a81b3f2ac7
Mike <michael.pineau89@gmail.com>
2025-01-13 15:17:14 +0000

    updated syntax and added in some schema details

---
+++ lmia/models/stg_lmia.sql
@@ -0,0 +1,17 @@
+with lmia as (
+    SELECT period as period,
+    program_stream as stream, 
+    employer as employer, 
+    split(address, ',')[0] as city, 
+    regex_substr(address, '(\D\d\D \D\d\D)', 1) as postal_code,
+    province as province, 
+    regexp_substr(occupation, '(\\d*)(?:-)', 1) as occupation_code,
+    regexp_substr(occupation, '(?:-)(.*)', 1) as occupation_name,
+    approved_lmia_count as approved_lmias,
+    approved_positions as approved_positions
+    FROM lmia.lmia_applications_raw
+    WHERE occupation is not null
+    and address is not null
+)
+
+select * from lmia
