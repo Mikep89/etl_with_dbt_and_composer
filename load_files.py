22aa7e6400041dd14ae7693db29888a3e45352aa
Mike <michael.pineau89@gmail.com>
2025-01-13 01:07:23 +0000

    dev
    updated variable call to fix gcs_to_bq

--- load_files.py
+++ load_files.py
@@ -13,7 +13,7 @@ schedule = "@once")
 def load_files():
     @task(task_id = 'get_raw_file')
     def get_raw_file(quarter: int) -> None:
-        bucket = "gs://" + Variable.get('test_gs_bucket')
+        bucket = "gs://" + Variable.get('test_gs_bucket') + '/'
         (pd.read_excel(bucket + f'raw_excel_lmia/tfwp_2024q{quarter}_pos_en.xlsx', skipfooter=8)
                 .to_csv(bucket + f'transformed_csv/lmia_q{quarter}.csv', index = False))
 
